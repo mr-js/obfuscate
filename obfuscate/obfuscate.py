da2f6f1e9d5d8d4ccb31f64eaa436a3972b8fdd7
mr-js <hello@mr-js.ru>
2024-12-27 07:07:00 +0300

    RC 2.0
    Compress photos

--- obfuscate/obfuscate.py
+++ obfuscate/obfuscate.py
@@ -2,12 +2,14 @@ import os
 import random
 import string
 import hashlib
+from PIL import Image
+import shutil
 
 
 bits = 8
 
 
-def obfuscate(path, save_order = True, write_changes = True):
+def obfuscate(path, obfuscate_filenames = True, save_order = True, compress_quality = 100, write_changes = False):
     for path, folders, files in os.walk(path):
         files.sort(key=lambda f: int(''.join(filter(str.isdigit, f)) or -1))
         index = 0
@@ -18,19 +20,35 @@ def obfuscate(path, save_order = True, write_changes = True):
         else:
             print(f'{path} files total: {total}')
         for file in files:
-            name, ext = os.path.splitext(os.path.join(path, file))
-            if not save_order:
-                mask = string.digits + string.ascii_uppercase
-                target = ''.join(random.choice(mask) for i in range(bits)) + str(ext).lower()
+            if obfuscate_filenames:
+                name, ext = os.path.splitext(os.path.join(path, file))
+                if not save_order:
+                    mask = string.digits + string.ascii_uppercase
+                    target = ''.join(random.choice(mask) for i in range(bits)) + str(ext).lower()
+                else:
+                    mask = string.ascii_uppercase
+                    prefix_width = len(str(total))
+                    target = f'{index:0{prefix_width}d}' + ''.join(random.choice(mask) for i in range(bits-prefix_width)) + str(ext).lower()
             else:
-                mask = string.ascii_uppercase
-                prefix_width = len(str(total))
-                target = f'{index:0{prefix_width}d}' + ''.join(random.choice(mask) for i in range(bits-prefix_width)) + str(ext).lower()
+                target = file
             print(f'{file[:24]:<24} -> {target}')
             if write_changes:
-                os.rename(os.path.join(path, file), os.path.join(path, target))
+                # os.rename(os.path.join(path, file), os.path.join(path, target))
+                overwrite_file_without_metadata(os.path.join(path, file), os.path.join(path, target))
+                if compress_quality != 100 and (file.endswith('.jpg') or file.endswith('.jpeg')):
+                    compress_jpeg(os.path.join(path, target), compress_quality)
             index += 1
     return 0
+
+
+def overwrite_file_without_metadata(source_file, target_file):
+    try:
+        with open(source_file, 'rb') as src, open(target_file, 'wb') as dst:
+            shutil.copyfileobj(src, dst)
+        if target_file != source_file:
+            os.remove(source_file)
+    except Exception as e:
+        print(f'Obfuscate file error {source_file} => {target_file}: {e}')
 
 
 def check_dublicates(path):
@@ -49,10 +67,39 @@ def check_dublicates(path):
     return dublicate_counter
 
 
+def compress_jpeg(path, compress_quality = 85):
+    try:
+        with Image.open(path) as img:
+            img.save(path, "JPEG", quality=compress_quality)
+    except Exception as e:
+        print(f'Compress image error {path}: {e}')
+
+
 if __name__ == "__main__":
     while (True):
-        path = input('Input Path and press Enter to continue...\n')
-        save_order = bool(input('Keep the original alphabetical order? Press Enter to not keep (default) or type any value to keep\n'))
+        path = input('\nInput Path and press Enter to continue...\n')
+        while dublicates := check_dublicates(path) != 0:
+            if not bool(input('Duplicate files with different names were found in the specified path. Fix it and press Enter (RECOMMENDED) to repeat or type any value to continue anyway?\n')):
+                continue
+            else:
+                break
+        obfuscate_filenames = not bool(input('Enable obfuscation? Press Enter to enable random renaming files (RECOMMENDED) or type any value to disable (save original file names)\n'))
+        if obfuscate_filenames:
+            save_order = not bool(input('Keep the original alphabetical order? Press Enter to keep (RECOMMENDED) or type any value to not keep (random)\n'))
+        else:
+            save_order = True
+        compress_images = bool(input('Compress photos? Press Enter now to pass (RECOMMENDED) or type any value to compress photos\n'))
+        if compress_images:
+            try:
+                compress_quality = int(input('Setup compression quality level 1 .. 99 (85 RECOMMENDED) and press Enter\n'))
+                if not (1 <= compress_quality <= 99):
+                    print('Compression quality level out of range! Setting default value: 85')
+                    compress_quality = 85
+            except ValueError:
+                print('Compression quality level out of range! Setting default value: 85')
+                compress_quality = 85
+        else:
+            compress_quality = 100
+        print(f'{path=}, {obfuscate_filenames=}, {save_order=}, {compress_images=}, {compress_quality=}')
         write_changes = bool(input('Write changes? Press Enter now to preview only (RECOMMENDED) or type any value to write changes immediately\n'))
-        if check_dublicates(path) == 0:
-            obfuscate(path, save_order, write_changes)
+        obfuscate(path, obfuscate_filenames, save_order, compress_quality, write_changes)
